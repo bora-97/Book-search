8b77af79159571ed8299eb7f5f9ed70bc0d975cc
Bora Xhidra <xhidrabora@gmail.com>
2021-01-11 19:52:35 -0500

    heroku deploy

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -13,18 +12,12 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-
 app.use(routes);
 
-
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/googlebooks",
-  {
-    useCreateIndex: true,
-    useNewUrlParser: true
-  }
-);
-
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+});
 
 app.listen(PORT, () =>
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
