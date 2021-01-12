e6f62cb69063b4dc9c84d47ee1520b8a8febe311
Bora Xhidra <xhidrabora@gmail.com>
2021-01-12 13:23:00 -0500

    Changed Home

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -12,38 +12,39 @@ class Home extends Component {
   state = {
     books: [],
     q: "",
-    message: "Search For A Book To Begin!"
+    message: "Search For A Book To Begin!",
   };
 
-  handleInputChange = event => {
+  handleInputChange = (event) => {
     const { name, value } = event.target;
+    console.log(name);
     this.setState({
-      [name]: value
+      [name]: value,
     });
   };
 
   getBooks = () => {
     API.getBooks(this.state.q)
-      .then(res =>
+      .then((res) =>
         this.setState({
-          books: res.data
+          books: res.data,
         })
       )
       .catch(() =>
         this.setState({
           books: [],
-          message: "No New Books Found, Try a Different Query"
+          message: "No New Books Found, Try a Different Query",
         })
       );
   };
 
-  handleFormSubmit = event => {
+  handleFormSubmit = (event) => {
     event.preventDefault();
     this.getBooks();
   };
 
-  handleBookSave = id => {
-    const book = this.state.books.find(book => book.id === id);
+  handleBookSave = (id) => {
+    const book = this.state.books.find((book) => book.id === id);
 
     API.saveBook({
       googleId: book.id,
@@ -52,7 +53,7 @@ class Home extends Component {
       link: book.volumeInfo.infoLink,
       authors: book.volumeInfo.authors,
       description: book.volumeInfo.description,
-      image: book.volumeInfo.imageLinks.thumbnail
+      image: book.volumeInfo.imageLinks.thumbnail,
     }).then(() => this.getBooks());
   };
 
@@ -65,7 +66,9 @@ class Home extends Component {
               <h1 className="text-center">
                 <strong>(React) Google Books Search</strong>
               </h1>
-              <h2 className="text-center">Search for and Save Books of Interest.</h2>
+              <h2 className="text-center">
+                Search for and Save Books of Interest.
+              </h2>
             </Jumbotron>
           </Col>
           <Col size="md-12">
@@ -83,7 +86,7 @@ class Home extends Component {
             <Card title="Results">
               {this.state.books.length ? (
                 <List>
-                  {this.state.books.map(book => (
+                  {this.state.books.map((book) => (
                     <Book
                       key={book.id}
                       title={book.volumeInfo.title}
